1bcc4eda3227833901c344b7ef18bf42c269f7d7
HanashiroYuriku <devan051003@gmail.com>
2026-03-30 02:27:21 +0700

    fix: change certification path

--- data/resume.ts
+++ data/resume.ts
@@ -10,7 +10,7 @@ export const resumeTimeline: TimelineItem[] = [
   { 
     date: "March 2026", 
     content: "Data Analyst Certification from Udemy. Learned how to analyze data and create visualizations.",
-    certificateSrc: "/UC-83c8a9b7-9757-49d8-ad5e-ac4f2f5d7fcf.jpg", 
+    certificateSrc: "/certificate/UC-83c8a9b7-9757-49d8-ad5e-ac4f2f5d7fcf.jpg", 
     certificateId: "UC-83c8a9b7-9757-49d8-ad5e-ac4f2f5d7fcf",
     certificateUrl: "https://www.udemy.com/certificate/UC-83c8a9b7-9757-49d8-ad5e-ac4f2f5d7fcf/"        
   },
@@ -21,7 +21,7 @@ export const resumeTimeline: TimelineItem[] = [
   { 
     date: "September 2024 - January 2025", 
     content: "Backend Developer Intern at Run System. Contributed to building scalable backend services and APIs." ,
-    certificateSrc: "/be-intern-sertif.jpg",
+    certificateSrc: "/certificate/be-intern-sertif.jpg",
     certificateId: "11417380"
   },
   { 
@@ -35,19 +35,19 @@ export const resumeTimeline: TimelineItem[] = [
   { 
     date: "April 2023 - June 2023", 
     content: "Arena Field Member in the Indonesian SAR Robot Contest. Complete obstacles by creating obstacles.", 
-    certificateSrc: "/arena-field-sertif.jpg",
+    certificateSrc: "/certificate/arena-field-sertif.jpg",
     certificateId: "[-]" 
   },
   { 
     date: "December 2022 - June 2023", 
     content: "Member of Java Programming Study. Explored the world of Java, from basic syntax to OOP concepts.", 
-    certificateSrc: "/java-programming-sertif.jpg",
+    certificateSrc: "/certificate/java-programming-sertif.jpg",
     certificateId: "[-]"
   },
   { 
     date: "August 2022 - June 2023", 
     content: "Member of the UAJY G-09 Robotics. Wrote my first lines of code to move a machine.", 
-    certificateSrc: "/robotics-sertif.jpg",
+    certificateSrc: "/certificate/robotics-sertif.jpg",
     certificateId: "[-]"
   }
 ];
