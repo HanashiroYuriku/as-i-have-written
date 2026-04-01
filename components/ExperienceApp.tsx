9ea63e8fbd9a1b0c73fe0ec73d786d3cb3d9b689
HanashiroYuriku <devan051003@gmail.com>
2026-04-02 01:07:09 +0700

    fix: fixed mid line of the book

--- components/ExperienceApp.tsx
+++ components/ExperienceApp.tsx
@@ -45,9 +45,9 @@ export default function ExperienceApp() {
   return (
     <div className="w-full h-full bg-[#ece5d3] text-[#4a3b32] flex flex-col md:flex-row overflow-y-auto md:overflow-hidden relative shadow-[inset_0_0_50px_rgba(0,0,0,0.1)]">
       
-      {/* --- KUNCI PERBAIKAN: Tulang Buku (Spine) --- */}
-      {/* Menggunakan -translate-x-1/2 agar presisi 100% membelah tengah */}
+      {/* batas tenah buku */}
       <div className="hidden md:block absolute left-1/2 top-0 bottom-0 w-10 -translate-x-1/2 bg-linear-to-r from-transparent via-[#c2a878]/40 to-transparent pointer-events-none z-20"></div>
+      <div className="hidden md:block absolute left-1/2 top-0 bottom-0 w-0.5 -translate-x-1/2 bg-[#d3c5af]/80 pointer-events-none z-20"></div>
 
       <AnimatePresence custom={direction} mode="wait">
         <motion.div 
@@ -59,9 +59,8 @@ export default function ExperienceApp() {
           exit="exit"
           className="flex flex-col md:flex-row w-full min-h-full"
         >
-          {/* --- HALAMAN KIRI (Teks) --- */}
-          {/* KUNCI PERBAIKAN: Mengganti flex-1 menjadi w-full md:w-1/2 */}
-          <div className="w-full md:w-1/2 p-6 md:p-12 flex flex-col relative border-b-2 md:border-b-0 md:border-r-2 border-[#d3c5af]/80 z-10">
+          {/* --- HALAMAN KIRI  --- */}
+          <div className="w-full md:w-1/2 p-6 md:p-12 flex flex-col relative border-b-2 md:border-b-0 border-[#d3c5af]/80 z-10">
             <div className="flex items-center justify-center md:justify-start gap-2 text-[#8b6b4a] opacity-80 mb-6 md:absolute md:top-8 md:left-8">
               <span className="w-8 h-px bg-[#8b6b4a]"></span>
               <span className="font-serif tracking-widest text-sm">Chapter {experiences.length - currentIndex}</span>
@@ -95,8 +94,7 @@ export default function ExperienceApp() {
             </div>
           </div>
 
-          {/* --- HALAMAN KANAN (Gambar/Slider) --- */}
-          {/* KUNCI PERBAIKAN: Mengganti flex-1 menjadi w-full md:w-1/2 */}
+          {/* --- HALAMAN KANAN --- */}
           <div className="w-full md:w-1/2 p-6 flex flex-col items-center justify-center relative bg-[#ece5d3] md:bg-transparent z-10">
             
             <div className="w-full max-w-sm md:max-w-md lg:max-w-md aspect-4/3 md:aspect-[1.4/1] bg-[#d3c5af] p-1.5 md:p-2 shadow-lg border border-[#c2a878]/50 relative group flex flex-col">
