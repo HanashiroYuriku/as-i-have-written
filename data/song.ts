e27c3773d1b8600b2dfc74b7fdd9561be275d29b
HanashiroYuriku <devan051003@gmail.com>
2026-04-04 19:14:15 +0700

    add: add new song

--- data/song.ts
+++ data/song.ts
@@ -5,12 +5,20 @@ export interface Song {
 
 export const playlist: Song[] = [
   {
-    title: "Ripples of Past Reverie (Original)",
+    title: "Ripples of Past Reverie",
     src: "/audio/ripples-original.mp3",
   },
   {
-    title: "Ripples of Past Reverie (Music Box)",
-    src: "/audio/ripples-music-box.mp3",
+    title: "(Instrument) With You Once More",
+    src: "/audio/with-you-once-more.mp3",
+  },
+  {
+    title: "(PV) With You Once More",
+    src: "/audio/with-you-once-more-pv.mp3",
+  },
+  {
+    title: "Night Falls Everyone, Close Your Eyes",
+    src: "/audio/night-falls-everyone.mp3",
   },
   {
     title: "Proi Proi",
