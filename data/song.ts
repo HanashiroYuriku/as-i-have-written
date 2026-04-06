22cd08183c61c88a64ff1e24ff4b29c3b6d99958
HanashiroYuriku <devan051003@gmail.com>
2026-04-07 02:47:39 +0700

    add: add new songs and change the queue

--- data/song.ts
+++ data/song.ts
@@ -1,27 +1,93 @@
 export interface Song {
   title: string;
   src: string;
+  artwork: string;
+  video: string;
 }
 
 export const playlist: Song[] = [
   {
+    title: "[VID]Ripples of Past Reverie",
+    src: "/audio/ripples-pv.mp3",
+    artwork: "/artworks/ripples-of-past-reverie.png",
+    video: "/video/ripples-pv.mp4",
+  },
+  {
+    title: "[VID]With You Once More",
+    src: "/audio/with-you-once-more-pv.mp3",
+    artwork: "/artworks/with-you-once-more.png",
+    video: "/video/with-you-once-more-pv.mp4",
+  },
+  {
+    title: "[VID]Nameless Faces",
+    src: "/audio/nameless-faces-pv.mp3",
+    artwork: "/artworks/nameless-faces.png",
+    video: "/video/nameless-faces-pv.mp4",
+  },
+  {
+    title: "[VID]Proi Proi",
+    src: "/audio/proi-proi-pv.mp3",
+    artwork: "/artworks/proi-proi.png",
+    video: "/video/proi-proi-pv.mp4",
+  },
+  {
+    title: "[VID]Nameless Faces JP",
+    src: "/audio/nameless-faces-jp.mp3",
+    artwork: "/artworks/nameless-faces.png",
+    video: "/video/nameless-faces-pv.mp4",
+  },
+  {
     title: "Ripples of Past Reverie",
     src: "/audio/ripples-original.mp3",
+    artwork: "/artworks/ripples-of-past-reverie.png",
+    video: "",
   },
   {
-    title: "(Instrument) With You Once More",
-    src: "/audio/with-you-once-more.mp3",
-  },
-  {
-    title: "(PV) With You Once More",
-    src: "/audio/with-you-once-more-pv.mp3",
-  },
-  {
-    title: "Night Falls Everyone, Close Your Eyes",
-    src: "/audio/night-falls-everyone.mp3",
+    title: "Nameless Faces",
+    src: "/audio/nameless-faces.mp3",
+    artwork: "/artworks/nameless-faces.png",
+    video: "",
   },
   {
     title: "Proi Proi",
     src: "/audio/proi-proi.mp3",
+    artwork: "/artworks/proi-proi.png",
+    video: "",
+  },
+  {
+    title: "With You Once More",
+    src: "/audio/with-you-once-more.mp3",
+    artwork: "/artworks/with-you-once-more.png",
+    video: "",
+  },
+  {
+    title: "Night Falls Everyone, Close Your Eyes",
+    src: "/audio/night-falls-everyone.mp3",
+    artwork: "/artworks/general-phainon.png",
+    video: "",
+  },
+  {
+    title: "Shatter The Gods Crown",
+    src: "/audio/shatter-the-gods-rown.mp3",
+    artwork: "/artworks/allegory.png",
+    video: "",
+  },
+  {
+    title: "Era Nova",
+    src: "/audio/era-nova.mp3",
+    artwork: "/artworks/general-phainon.png",
+    video: "",
+  },
+  {
+    title: "Nameless Healing",
+    src: "/audio/nameless-healing.mp3",
+    artwork: "/artworks/allegory.png",
+    video: "",
+  },
+  {
+    title: "Agony Converging Into River",
+    src: "/audio/agony-converging-into-river.mp3",
+    artwork: "/artworks/allegory.png",
+    video: "",
   },
 ];
