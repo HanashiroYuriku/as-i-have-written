e49a89a3a1993c28de71cd448b365d9f466ddab2
HanashiroYuriku <devan051003@gmail.com>
2026-04-07 02:47:01 +0700

    fix: change parameter from md to lg

--- components/RealTimeClock.tsx
+++ components/RealTimeClock.tsx
@@ -17,11 +17,11 @@ export default function RealTimeClock() {
         hour12: false
       });
       
-      // Format Panjang untuk Desktop (Contoh: SATURDAY, APRIL 4 2026)
+      // Format Panjang Desktop
       const weekdayLong = now.toLocaleDateString('en-US', { weekday: 'long' });
       const monthLong = now.toLocaleDateString('en-US', { month: 'long' });
       
-      // Format Pendek untuk Mobile (Contoh: SAT, APR 4 2026)
+      // Format Pendek Mobile
       const weekdayShort = now.toLocaleDateString('en-US', { weekday: 'short' });
       const monthShort = now.toLocaleDateString('en-US', { month: 'short' });
       
@@ -41,13 +41,15 @@ export default function RealTimeClock() {
 
   return (
     <>
-      <span className="uppercase text-slate-300 drop-shadow-sm whitespace-nowrap">
-        <span className="md:hidden text-xs tracking-wider">{shortDate}</span>
-        <span className="hidden md:inline text-xs md:text-sm tracking-widest">{longDate}</span>
+      <span className="uppercase text-slate-300 drop-shadow-sm whitespace-nowrap flex items-center pt-1 pb-1">
+        <span className="lg:hidden text-sm tracking-wider">{shortDate}</span>
+        
+        <span className="hidden lg:inline text-sm tracking-widest">{longDate}</span>
       </span>
       
-      <div className="w-px h-3 md:h-4 bg-slate-600 shrink-0"></div>
-      <span className="font-mono text-base md:text-lg text-white drop-shadow-[0_0_8px_rgba(255,255,255,0.5)] shrink-0">
+      <div className="w-px h-5 lg:h-6 bg-slate-600 shrink-0"></div>
+      
+      <span className="font-mono text-lg lg:text-md text-white drop-shadow-[0_0_8px_rgba(255,255,255,0.5)] shrink-0 flex items-center leading-none">
         {time}
       </span>
     </>
